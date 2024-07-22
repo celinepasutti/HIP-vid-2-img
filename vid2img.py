8d1411c00e8ade0f5750fd4016277990602520fa
celinepasutti <celinepasutti@gmail.com>
2024-07-22 13:35:43 -0600

    Update vid2img.py

--- vid2img.py
+++ vid2img.py
@@ -1,6 +1,6 @@
 import cv2
 
-def extract_and_crop_images_from_video(video_path, output_folder, frame_parameter):
+def extract_and_crop_images_from_video(video_path, output_folder, frame_parameter, crop):
     cap = cv2.VideoCapture(video_path)
     
     if not cap.isOpened():
@@ -16,29 +16,32 @@ def extract_and_crop_images_from_video(video_path, output_folder, frame_paramete
         if not ret:
             break
         
-        # crop to a square
-        height, width, _ = frame.shape
-        if height > width: # vertical
-            offset = (height - width) // 2
-            square_frame = frame[offset:offset+width, :]
-        else: # horizontal
-            offset = (width - height) // 2
-            square_frame = frame[:, offset:offset+height]
+        if crop:
+            # crop to a square
+            height, width, _ = frame.shape
+            if height > width:  # vertical
+                offset = (height - width) // 2
+                square_frame = frame[offset:offset+width, :]
+            else:  # horizontal
+                offset = (width - height) // 2
+                square_frame = frame[:, offset:offset+height]
+        else:
+            square_frame = frame
         
         if frame_count % frame_parameter == 0:
             output_path = f"{output_folder}/frame_{frame_count}.jpg"
             cv2.imwrite(output_path, square_frame)
             extracted_count += 1
-            print(f"Extracted and cropped frame {frame_count} to {output_path}")
+            print(f"Extracted frame {frame_count} to {output_path}")
         
         frame_count += 1
     
     cap.release()
     print(f"Extraction complete. Extracted {extracted_count} frames.")
 
+# usage
 video_path = './media/videos/makeup.MOV'
 output_folder = './media/images'
 frame_parameter = 10
 
-extract_and_crop_images_from_video(video_path, output_folder, frame_parameter)
-
+extract_and_crop_images_from_video(video_path, output_folder, frame_parameter, crop=False)
